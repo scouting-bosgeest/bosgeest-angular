523c1bf997b7b2d4b13f9a272be622bc02e56eb2
Oscar Farg <oscar.farg@onior.com>
2018-02-01 11:51:10 +0100

    Added modal to add custom track

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, TemplateRef} from '@angular/core';
 import {Subject} from 'rxjs/Subject';
 import {Observable} from 'rxjs/Observable';
 import {
@@ -7,6 +7,8 @@ import {
 import {SearchService} from '../search.service';
 import {Track} from '../track';
 import {TrackListService} from '../track-list.service';
+import {BsModalRef} from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import {BsModalService} from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-search',
@@ -16,13 +18,24 @@ import {TrackListService} from '../track-list.service';
 export class SearchComponent implements OnInit {
 
   private searchTerms = new Subject<string>();
-
+  modalRef: BsModalRef;
   tracks$: Observable<any>;
-
-  constructor(private searchService: SearchService, public trackListService: TrackListService) { }
+  constructor(private searchService: SearchService, public trackListService: TrackListService, private modalService: BsModalService) { }
 
   search(query: string): void {
     this.searchTerms.next(query);
+  }
+
+  openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  addTrack(artist: string, title: string) {
+      const track = new Track();
+      track.artist = artist;
+      track.title = title;
+      this.trackListService.add(track);
+      this.modalRef.hide();
   }
 
   ngOnInit(): void {
