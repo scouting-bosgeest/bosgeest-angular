ff58c13949d07a7b59c065b95767add77f4bb35c
Oscar Farg <oscar.farg@onior.com>
2018-02-08 10:55:50 +0100

    small optimalizations

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
-import { catchError, map, tap } from 'rxjs/operators';
+import {Observable} from 'rxjs/Observable';
+import {of} from 'rxjs/observable/of';
+import {catchError} from 'rxjs/operators';
 
-import { Track} from './track';
+import {Track} from './track';
 import {environment} from '../environments/environment';
 
 const httpOptions = {
